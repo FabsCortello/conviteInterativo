8bf43c5096af0d4aa8b8c6b117c746157adc95cb
Fabiana Cortello <biofabi@gmail.com>
2024-09-06 13:39:15 -0300

    Add files via upload

---
+++ java/app.js
@@ -0,0 +1,7 @@
+function abrirPagina() {
+    window.location.href = 'convite.html'; // Substitua com o caminho da página desejada
+}
+function abrirFormulario() {
+    
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLSew1QzHwz6yd2-RpCK3rKJCFhq7MkQkaj2G2ZzEP78WA0OOeg/viewform?embedded=true", '_blank');
+}
